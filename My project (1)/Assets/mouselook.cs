0fbd8c58c8e0dca05fd2d39ea182b3e78a73bf1e
LoFu202 <michiderluca@gmail.com>
2025-07-15 22:26:30 +0200

    Mouselook
    added mouselook script to actually look around while moving.

---
+++ My project (1)/Assets/mouselook.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class Mouselook : MonoBehaviour
+{
+    public Transform playerBody;
+    public float mouseSensitivity = 100f;
+    float xRotation = 0f;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -75f, 75f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+
+
+    public void SetMouseSensitivity(float newSensitivity)
+    {
+        mouseSensitivity = newSensitivity;
+    }
+}
